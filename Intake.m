bb6c30f3c84d6e8a7a5f366386bf7dbe70fcf5d5
kasparsvv <k.veveris@student.tue.nl>
2024-02-26 18:59:01 +0100

    a

--- Intake.m
+++ Intake.m
@@ -22,27 +22,56 @@ P = ones(1, 181) * P_atm; % Constant pressure throughout intake stroke
 
 %% Plotting different figures
 
-% PV
-figure;
-plot (Vcyl, P) ;
-xlabel ( ' Volume ( m ^3) ') ;
-ylabel ( ' Pressure ( Bar ) ') ;
-title ( 'Intake stroke') ;
-grid on ;
+% % PV
+% figure;
+% plot (Vcyl, P) ;
+% xlabel ( ' Volume ( m ^3) ') ;
+% ylabel ( ' Pressure ( Bar ) ') ;
+% title ( 'Intake stroke') ;
+% grid on ;
+% 
+% % Theta V
+% figure;
+% plot (theta_deg, Vcyl) ;
+% xlabel ( '\theta (degrees)') ;
+% ylabel ( 'Specific volume [m^3]') ;
+% title ( 'Intake stroke') ;
+% grid on ;
+% 
+% % Theta x
+% figure;
+% plot (theta_deg, x) ;
+% xlabel ( '\theta (degrees)') ;
+% ylabel ( ' x [m] ') ;
+% title ( 'Intake stroke') ;
+% grid on ;
 
-% Theta V
-figure;
-plot (theta_deg, Vcyl) ;
-xlabel ( '\theta (degrees)') ;
-ylabel ( 'Specific volume [m^3]') ;
-title ( 'Intake stroke') ;
-grid on ;
 
-% Theta x
-figure;
-plot (theta_deg, x) ;
-xlabel ( '\theta (degrees)') ;
-ylabel ( ' x [m] ') ;
-title ( 'Intake stroke') ;
-grid on ;
+% Compression stroke, ignition and Power Stroke
+
+C1 = p(1) * V(1) ^ gamma; % Poisson relations
+C2 = T(1) * V(1) ^(gamma - 1); % Poisson relations
+
+% Compression
+if Ca(i) >= -180 && Ca(i) < 0
+    p(i) = C1 / V(i) ^(gamma); % Poisson relations
+    T(i) = C2 / V(i) ^(gamma - 1); % Poisson relations
+end
+
+% Ignition
+if Ca(i) == 0
+    dQcom(i) = 730; % Heat Release by combustion
+    dT(i) = (dQcom(i) - p(i - 1) * dV) / Cv / m(i - 1); % 1st Law dU=dQ -pdV (closed system)
+    T(i) = T(i - 1) + dT(i);
+    p(i) = m(i) * Rg * T(i) / V(i); % Gaslaw
+    C3 = p(i) * V(i) ^ gamma; % Poisson relations
+    C4 = T(i) * V(i) ^(gamma - 1); % Poisson relations
+end
+
+% Power stroke
+if Ca(i) > 0 && Ca(i) < 180
+    p(i) = C3 / V(i) ^(gamma); % Poisson relations
+    T(i) = C4 / V(i) ^(gamma - 1); % Poisson relations
+end
+
 
