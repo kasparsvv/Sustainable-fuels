6fc090b92578700a0f626315540d3e02af54036e
kasparsvv <k.veveris@student.tue.nl>
2024-02-26 03:06:15 +0100

    Added functions to plot graphs

--- exhaust.m
+++ exhaust.m
@@ -11,11 +11,37 @@ P_atm  = 1; % [Bar] Atmospheric pressure
 
 %% Defining the kinematic equations of the engine as functions of theta
 
-theta_deg = linspace(-15, 215, 231); % Intake stroke in degrees. Lasts from -15 to 215
+theta_deg = linspace(180, 0, 181); % Intake stroke in degrees. Lasts from -15 to 215
 theta_rad = deg2rad(theta_deg); % Intake stroke in radians for ease of use
 
 x = r * cos(theta_rad) + sqrt(l^2 - r^2 * sin(theta_rad).^2); % [m] x postion of piston as a function of theta
 d = l + r - x; % [m] Distance of pistion from TDC as a function of theta
 Vcyl = pi * (B/2)^2 * d + V_c; % [m^3] Free cylinder volume as a function of theta
 
-P = ones(1, 231) * P_atm; % Constant pressure throughout intake stroke
+P = ones(1, 181) * P_atm; % Constant pressure throughout intake stroke
+
+%% Plotting different figures
+
+% PV
+figure;
+plot (Vcyl, P) ;
+xlabel ( ' Volume ( m ^3) ') ;
+ylabel ( ' Pressure ( Bar ) ') ;
+title ( 'Exhaust stroke') ;
+grid on ;
+
+% Theta V
+figure;
+plot (theta_deg, Vcyl) ;
+xlabel ( '\theta (degrees)') ;
+ylabel ( 'Specific volume [m^3]') ;
+title ( 'Exhaust stroke') ;
+grid on ;
+
+% Theta x
+figure;
+plot (theta_deg, x) ;
+xlabel ( '\theta (degrees)') ;
+ylabel ( ' x [m] ') ;
+title ( 'Exhaust stroke') ;
+grid on ;
