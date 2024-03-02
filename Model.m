0c9d5079d9fd912f2f23ad3184cb4487e13ee045
20223628 <20223628@S20223628.campus.tue.nl>
2024-03-02 16:14:51 +0100

    Otto cycle almost finished

--- Model.m
+++ Model.m
@@ -1,3 +1,4 @@
+clear all;
 %% Load the NASA tables
 
 relativepath_to_generalfolder='General'; % relative reference to General folder (assumes the folder is in you working folder)
@@ -5,8 +6,13 @@ addpath(relativepath_to_generalfolder);
 % Load Nasadatabase
 TdataBase=fullfile('General','Nasa','NasaThermalDatabase');
 load(TdataBase);
+run("AFcalculations.m");
+run("Fraction_calculations.m");
 
 %% Constants
+
+global Runiv
+Runiv=8.314472;
 
 % Crank angle steps
 NCa=720;                % Number of crank-angles
@@ -93,28 +99,27 @@ MolesGasoline = 1/MGasoline;                  % Amount of moles gasoline per mas
 MolesOxygenG = MolesGasoline*12.5;              % Amount of moles Oxygen for this amount of gasoline
 MassOxygenG = MolesOxygenG*MiGasoline(2);    % The mass of this oxygen
 MassAirG = MassOxygenG + (MassOxygenG/Yair(2))*Yair(5); % Mass of air (Oxygen + Nitrogen)
-AirFuelRatioGasoline = MassAirG / 1;         % Air-fuel ratio for Gasoline 
+%AirFuelRatioGasoline = MassAirG / 1;         % Air-fuel ratio for Gasoline 
 
 
 
 VolumeEthanol = Evalue/100;
 VolumeGasoline = (100-Evalue)/100;
 
-MassEthanol = 789*VolumeEthanol;            % Ethanol = 789 kg/m^3
-MassGasoline = 749*VolumeGasoline;          % Gasoline = 749 kg/m^3   
+MassEthanol = DensityEthanol*VolumeEthanol;            % Ethanol = 789 kg/m^3
+MassGasoline = DensityGasoline*VolumeGasoline;          % Gasoline = 749 kg/m^3   
 %Mass fractions
-MassFractionEthanol = MassEthanol/(MassEthanol+MassGasoline);
-MassFractionGasoline = MassGasoline/(MassEthanol+MassGasoline);
+%MassFractionEthanol = MassEthanol/(MassEthanol+MassGasoline);
+%MassFractionGasoline = MassGasoline/(MassEthanol+MassGasoline);
 
-MFuel = MassFractionEthanol*MEthanol + MassFractionGasoline*MGasoline;      % Molar mass of the fuel mixture
-mfuel = 1;               %Fuel mass inside the cylinder, used for Qmodel, not defined yet
+%MFuel = MassFractionEthanol*MEthanol + MassFractionGasoline*MGasoline;      % Molar mass of the fuel mixture
+%mfuel = 1;               %Fuel mass inside the cylinder, used for Qmodel, not defined yet
 
-AirFuelRatio = MassFractionEthanol*AirFuelRatioEthanol + MassFractionGasoline*AirFuelRatioGasoline;     % Air-fuel ratio for the fuel mixture 
-MFuelAir = MFuel + AirFuelRatio*MFuel;          % Mass of air = Air-Fuel ratio * Mass of the fuel
+%AirFuelRatio = MassFractionEthanol*AirFuelRatioEthanol + MassFractionGasoline*AirFuelRatioGasoline;     % Air-fuel ratio for the fuel mixture 
+%MFuelAir = MFuel + AirFuelRatio*MFuel;          % Mass of air = Air-Fuel ratio * Mass of the fuel
 
-Rg = Runiv/MFuelAir;   %Specific gas constant
+%Rg = Runiv/MFuelAir;   %Specific gas constant
 %Rg = 290;
-
 
 %% Initialisation
 
@@ -138,11 +143,11 @@ m(1) = p(1)*V(1)/Rg/T(1);
 % Cp = Yi_before * Cpi';
 
 % gamma = Cp/Cv;
-Cv = 1000;
+%Cv = 1000;
 
-gamma = 1.4;
-C1 = p(1)*V(1)^gamma;       % Poisson relations
-C2 = T(1)*V(1)^(gamma-1);   % Poisson relations
+%gamma = 1.4;
+%C1 = p(1)*V(1)^gamma;       % Poisson relations
+%C2 = T(1)*V(1)^(gamma-1);   % Poisson relations
 
 %% Loop over the crank angles using a For-loop
 
@@ -155,26 +160,43 @@ for i=2:NSteps                          % Calculate values for 1 cycle
     % Intake
     if Ca(i) >= 0 && Ca(i) < 180
         p(i) = p0;
+        T(i) = T0;
     end
+    for n=1:5
+            Cvi(n) = CvNasa(T(360),SpSGasoline(n));
+            Cpi(n) = CpNasa(T(360),SpSGasoline(n));
+    end
+    %Cv_comp_in = Y_comp_in.*Cvi'
+    %Cp_comp_in = Y_comp_in.*Cpi'
+
+    Cv_comp_in = dot(Y_comp_in,Cvi);
+    Cp_comp_in = dot(Y_comp_in,Cpi);
+
+    gamma_comp_in = Cp_comp_in/Cv_comp_in;
+
 
     % Compression
     if Ca(i) >= 180 && Ca(i) < 360
-        p(i) = C1/V(i)^(gamma);         % Poisson relations
-        T(i) = C2/V(i)^(gamma-1);       % Poisson relations
+        C1 = p(360)*V(360)^gamma_comp_in;
+        C2 = T(360)*V(360)^(gamma_comp_in-1);
+
+        
+        p(i) = C1/V(i)^(gamma_comp_in);         % Poisson relations
+        T(i) = C2/V(i)^(gamma_comp_in - 1);       % Poisson relations
     end
 
     % Ignition
     if Ca(i) == 360
+        for n=1:5
+        Cvi_comb_in(n) =CvNasa(T(720),SpSGasoline(n));           % Get Cv from Nasa-table
+        end
+        Cv_comb_in = dot(Y_comp_in,Cvi_comb_in);
         dQcom(i) = 730;                         % Heat Release by combustion
-        dT(i)=(dQcom(i)-p(i-1)*dV)/Cv/m(i-1);   % 1st Law dU=dQ-pdV (closed system)
+        dT(i)=(dQcom(i)-p(i-1)*dV)/Cv_comb_in/m(i-1);   % 1st Law dU=dQ-pdV (closed system)
         T(i)=T(i-1)+dT(i);
         p(i)=m(i)*Rg*T(i)/V(i);                 % Gaslaw
 
-        % for n=1:NSp
-        % Cvi(n) =CvNasa(T(i),SpS(n));           % Get Cv from Nasa-table
-        % end
-        % Cv = Yi_after * Cvi';
-
+        
         % for n=1:NSp
         % Cpi(n) =CpNasa(T(i),SpS(n));           % Get Cp from Nasa-table
         % end
@@ -182,16 +204,31 @@ for i=2:NSteps                          % Calculate values for 1 cycle
 
         % gamma = Cp/Cv;
 
-        C3 = p(i)*V(i)^gamma;                   % Poisson relations
-        C4 = T(i)*V(i)^(gamma-1);               % Poisson relations
+        
     end
+    for n=1:5
+    Cvi_comb_out(n) = CvNasa(T(721),SpSGasoline(n));
+    Cpi_comb_out(n) = CpNasa(T(721),SpSGasoline(n));
+    end
+    %Cv_comp_in = Y_comp_in.*Cvi'
+    %Cp_comp_in = Y_comp_in.*Cpi'
+
+    Cv_comb_out = dot(Y_comb_out,Cvi_comb_out);
+    Cp_comb_out = dot(Y_comb_out,Cpi_comb_out);
+
+    gamma_comb_out = Cp_comb_out/Cv_comb_out;
 
     % Power stroke
     if Ca(i) > 360 && Ca(i) < 540
-        p(i) = C3/V(i)^(gamma);         % Poisson relations
-        T(i) = C4/V(i)^(gamma-1);       % Poisson relations
+        C3 = p(721)*V(721)^gamma_comb_out;
+        C4 = T(721)*V(721)^(gamma_comb_out-1);
+        p(i) = C3/V(i)^(gamma_comb_out);         % Poisson relations
+        T(i) = C4/V(i)^(gamma_comb_out-1);       % Poisson relations
     end
-
+    for n=1:5
+        Cvi_ps_out(n) =CvNasa(T(1080),SpSGasoline(n));           % Get Cv from Nasa-table
+    end
+    Cv_ps_out = dot(Y_comb_out,Cvi_ps_out);
     % Heat release
     if Ca(i) == 540      
 
@@ -199,12 +236,13 @@ for i=2:NSteps                          % Calculate values for 1 cycle
 
         p(i) = p0;         
         T(i) = T0;  
-        Q_c = Cv * m(i) * (T(i)-T(i-1));
+        Q_c = Cv_ps_out * m(i) * (T(i)-T(i-1));
     end
 
     % Exhaust
     if Ca(i) >= 540 && Ca(i) <= 720
         p(i) = p0;
+        T(i) = T0;
     end
     
 end
