ddb4f1f51e17e4484d0e40f53551903f3ace8882
kasparsvv <k.veveris@student.tue.nl>
2024-03-04 15:11:50 +0100

    Removed the system parameters and moved them to a different file to tidy up the code. Added run("system_parameters")

--- Model.m
+++ Model.m
@@ -8,46 +8,12 @@ TdataBase=fullfile('General','Nasa','NasaThermalDatabase');
 load(TdataBase);
 run("AFcalculations.m");
 run("Fraction_calculations.m");
+run("system_parameters.m")
 
 %% Constants
 
 global Runiv
 Runiv=8.314472;
-
-% Crank angle steps
-NCa=720;                % Number of crank-angles
-dCa=0.5;                % Stepsize
-NSteps=NCa/dCa;
-
-% Create arrays
-p = zeros(1, NSteps);
-V = zeros(1, NSteps);
-T = zeros(1, NSteps);
-dQcom = zeros(1, NSteps);
-dT = zeros(1, NSteps);
-m = zeros(1, NSteps);
-
-% System constant
-p0 = 1.01235e5 ;        % Ambient Pressure (pa)
-T0 = 293;               % Ambient Temparature (K)
-rc = 8.5;               % Compression ratio (-)
-Runiv = 8.314472;       % Universal gas constant (J / mol·K)
-CaS = 210;              % Crank angle at start of combustion
-CaD = 225-210;          % Combustion duration (in terms of crank angle)
-n = 3;                  % Wiebe form factor, Project handbook says 3 is often used
-a = 5;                  % Wiebe efficiency factor, Project handbook says 5 is often used
-
-S = 0.055;              % [m] Stroke
-r  = 1/2 * S;           % [m] Length of crankshaft
-l = 0.0842;             % [m] Length of the connecting rod
-B = 0.0677;             % [m] Bore dimension
-TDC = 0.003;            % [m] Top dead center
-BDC = S + TDC;          % [m] Bottom dead center
-V_c = pi/4 * B^2 * TDC; % [m63] Compression volume (will change)
-P_atm  = 1;             % [Bar] Atmospheric pressure (assumed for now)
-Ca(1) = 0;              % Initial crank angle
-
-S_p = 2 * 3000/60 * S;
 
 %% Fuel computations
 
@@ -132,9 +98,11 @@ pad(1) = p(1);
 Ca(1) = 0;
 V(1) = Vcyl(Ca(1),S,B,l,rc); % Vcyl is a function that computes cylinder volume as function of crank-angle for given B,S,l and rc
 m(1) = p(1)*V(1)/Rg_before_comb/T(1);
-P_ref = 1.859606618881350e+06;
-V_ref = 2.640285571312627e-05;
-T_ref = 6.333244594935253e+02;
+
+P_ref = 1.859606618881350e+06; % [Pa] Reference pressure taken at crangle angle of 360
+V_ref = 2.640285571312627e-05; % [Pa] Reference volume  taken at crangle angle of 360
+T_ref = 6.333244594935253e+02; % [Pa] Reference temperature taken at crangle angle of 360
+
 %% Poisson relations
 
 % for n=1:NSp
@@ -281,11 +249,17 @@ for i=2:NSteps                          % Calculate values for 1 cycle
         m(i) = p(i)*V(i)/Rg_after_comb/T(i);
     end
 
+
+    % A(i) = (pi/2)*B^2 + pi*B*(r*cosd(Ca(i)) + sqrt(l^2 - r^2*(sind(Ca(i))^2)));
+    % Q_loss(i) = h_woschni(i) * A(i) * (T(i) - 330);
+
+    % p_motor1(i) = (((rc *  max(V)/(rc - 1))^gamma_comb_out * P_atm)/V(i)^gamma_comb_out); % [Pa] motorized cylinder pressure
+
     p_motor2(i) = (P_ref * (V_ref/V(i))^gamma_comb_out);
 
-    w(i) = B1(i)*S_p + B2(i)*((max(V)*T_ref)/(P_ref*V_ref)) * (p(i) - p_motor2(i));
+    w(i) = B1(i)*S_p + B2(i)*((max(V)*T_ref)/(P_ref*V_ref)) * (p(i) - p_motor2(i)); % [m/s] Effective gas velocity
 
-    h_woschni(i) = 3.26 * B^(-0.2) * p(i)^(0.8) * T(i)^(-0.55) * w(i)^0.8;
+    h_woschni(i) = 3.26 * B^(-0.2) * p(i)^(0.8) * T(i)^(-0.55) * w(i)^0.8; % [W/(m^2*K)]
     h_hohenberg(i) = 140 * V(i)^(-0.06) * p(i)^(0.8) * T(i)^(-0.4) * (S_p + 1.4)^(0.8);
     h_eichelberg(i) = 7.799 * 10^(-3) * S_p^(1/3) * p(i)^(0.5) * T(i)^(0.5);
 
