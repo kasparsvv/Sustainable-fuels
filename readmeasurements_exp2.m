57e942758a5e316ced86004ddc6a436b5b595b21
20221847 <20221847@S20221847.campus.tue.nl>
2024-03-05 19:50:18 +0100

    changed the code so that we find the double tooth separately

--- readmeasurements_exp2.m
+++ readmeasurements_exp2.m
@@ -2,7 +2,8 @@ clear all;close all;clc
 %% add general to matlab path 
 addpath('General');
 %%
-DataDir='FULL';ColumnOrder={'time','Sensor','Encoder'};
+DataDir='';ColumnOrder={'time','Sensor','Encoder'}; %here the directory should be to a folder with just one measurement for a fuel mixture
+% at half no and full load
 % DataDir='../Data/Gasoline';cOrder={'time'','Encoder','Sensor};
 col = lines(3);
 %% Measurements
@@ -14,15 +15,15 @@ col = lines(3);
 %% Loading all measurments in DataDir
 figure(1)
 Files=dir(fullfile(DataDir,'*.txt'));nFiles=length(Files);                  % dir gives a directory listing, only *.txt files in this case
-for i=1:nFiles
-    fname       = Files(i).name;                                            % Take a name from the list
+for k=1:nFiles
+    fname       = Files(k).name;                                            % Take a name from the list
     curfilename = fullfile(DataDir,fname);                                  % Create the full name
     Data        = ImportData4GB10(curfilename,ColumnOrder);                             % Read the data. Type help ImportData4GB10
     % store it for each case. Yes a struct that contains a struct and other
     % things. Why not?
-    Case(i).Data     = Data;
-    Case(i).filename = fname;
-    Case(i).DataDir  = DataDir;
+    Case(k).Data     = Data;
+    Case(k).filename = fname;
+    Case(k).DataDir  = DataDir;
     % preamble, put data in easy to use arrays
     t      = Data.t; %time array
     p      = Data.pulse; %pulse array
@@ -32,21 +33,25 @@ for i=1:nFiles
 %%
     pos1 = RevEnd(1);                                                   
     pos2 = RevEnd(3); %a complete cycle includes 2 revolutions
-    V_cycle = V(pos1:pos2);                                           
-    p_max = max(V_cycle);  %find max pressure value in the cycle 
+    %V_cycle = V(pos1:pos2);                                           
+    %p_max = max(V_cycle);  %find max pressure value in the cycle 
 
-    position_max_pressurei = find(V_cycle==p_max);        
-    position_max_pressure = position_max_pressurei(1);
+    %position_max_pressurei = find(V_cycle==p_max);        
+    %position_max_pressure = position_max_pressurei(1);
     
-    Doubletooth = deg2rad(16) - (position_max_pressure-1)*(2*pi/((pos2 - pos1+1)/2));  
+    %Doubletooth = deg2rad(16) - (position_max_pressure-1)*(2*pi/((pos2 - pos1+1)/2));  
+    Doubletooth=deg2rad(-223); %put in double tooth angle
+    pos1 = RevEnd(1);                                                   
+    pos2 = RevEnd(3); %a complete cycle includes 2 revolutions
 
     Ca(1) = Doubletooth; %first crank angle is at double tooth
     Ca(RevEnd(1)) = Doubletooth; %crank angle at the end of a revolution is also at the double tooth
-    dCa = 2*pi/((pos2-pos1+1)/2); %how much the crank angle varies per index                                 
+    dCa = 2*pi/((pos2-pos1+1)/2); %how much the crank angle varies per index 
+    Ca(RevEnd(1)-1) = Doubletooth - dCa;
     Volume(RevEnd(1)) = Vcyl(Ca(1),S,B,l,rc); %volume at the end of a revolution is found using the Vcyl function
     Pressure(RevEnd(1)) = ((V(RevEnd(1)))/5-0.115)/0.00385;
    
-    figure(i)  
+    figure(k)  
     for j=3:2:(length(RevEnd)-1)                                                
         for i=(RevEnd(j-2)+1):RevEnd(length(RevEnd))                         
             if i~=RevEnd(j)                                                  
@@ -60,7 +65,14 @@ for i=1:nFiles
             xlabel('Volume [m^3]')
             ylabel('Pressure [bar]')
             legend('Real cycle')
-            title('Full load')      
+            if k==1
+                title('No load')
+            else 
+                if k==2
+                    title('Full load')
+            else title('Half load')
+                end    
+            end
             hold on
             Ca(i) = Ca(1);
             Volume = [];
